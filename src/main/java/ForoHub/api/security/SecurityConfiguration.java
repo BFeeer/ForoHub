d1fd0c7c69ccf10f6161aea400ecc87856016ece
BFeer <bfmg69@gmail.com>
2024-07-16 21:59:01 -0600

    Autenticación básica de usuario en memoria

--- src/main/java/ForoHub/api/security/SecurityConfiguration.java
+++ src/main/java/ForoHub/api/security/SecurityConfiguration.java
@@ -2,23 +2,60 @@ package ForoHub.api.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-    @Bean
-    // configuración de seguridad
+    @Bean // configuración de la cadena de filtros de seguridad
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity
-                .authorizeHttpRequests(auth ->{ // configuración de seguridad de urls
+                .csrf(AbstractHttpConfigurer::disable) // protección contra cross-site request forgery (desactivado)
+                .authorizeHttpRequests(auth ->{ // configuración de acceso a urls
                     auth.requestMatchers("/autenticacion").permitAll();
                     auth.anyRequest().authenticated();
                 })
-                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session ->  // configurar el comportamiento de la sesión
+                        // política de creación de sesión
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .httpBasic(Customizer.withDefaults()) // establecer autenticación básica
                 .build(); // construir el objeto
+    }
+
+    @Bean // configuración de un usuario
+    public UserDetailsService userDetailsService(){
+        // crear un usuario en memoria
+        UserDetails adminUser = User.builder()
+                .username("fer")
+                .password(passwordEncoder().encode("123"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(adminUser);
+    }
+
+    @Bean // definición de un encriptador de contraseñas
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean // definición de un administrador de autenticación
+    public AuthenticationManager authenticationManager(HttpSecurity httpSecurity) throws Exception {
+        AuthenticationManagerBuilder authManagerBuilder = httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
+        authManagerBuilder.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+        return authManagerBuilder.build();
     }
 }
