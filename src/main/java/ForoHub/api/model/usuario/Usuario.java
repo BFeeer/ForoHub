a6909a625da4f46ae2024ee91cf141fc91f9d45a
BFeer <bfmg69@gmail.com>
2024-07-18 13:35:02 -0600

    Actualización de entidad Usuario

--- src/main/java/ForoHub/api/model/usuario/Usuario.java
+++ src/main/java/ForoHub/api/model/usuario/Usuario.java
@@ -3,16 +3,15 @@ package ForoHub.api.model.usuario;
 import ForoHub.api.model.respuesta.Respuesta;
 import ForoHub.api.model.tema.Tema;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
+@Data
+@Builder
 @Entity
 @Table(name = "usuarios")
 public class Usuario {
@@ -36,27 +35,4 @@ public class Usuario {
     // un usuario puede tener múltiples respuestas
     @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
     private List<Respuesta> respuestas;
-
-
-    // constructor
-    public Usuario(DatosRegistrarUsuario datosRegistrarUsuario) {
-        this.nombreUsuario = datosRegistrarUsuario.nombreUsuario();
-        this.contrasenia = datosRegistrarUsuario.contrasenia();
-        this.correoElectronico = datosRegistrarUsuario.correoElectronico();
-        this.fechaCreacion = LocalDateTime.now();
-        this.fechaActualizacion = LocalDateTime.now();
-    }
-
-    // métodos
-    public void actualizar(DatosActualizarUsuario datosActualizarUsuario) {
-        if (datosActualizarUsuario.nombreUsuario() != null){
-            this.nombreUsuario = datosActualizarUsuario.nombreUsuario();
-        }
-        if (datosActualizarUsuario.contrasenia() != null){
-            this.contrasenia = datosActualizarUsuario.contrasenia();
-        }
-        if ((datosActualizarUsuario.nombreUsuario() != null)||(datosActualizarUsuario.contrasenia() != null)) {
-            this.fechaActualizacion = LocalDateTime.now();
-        }
-    }
 }
