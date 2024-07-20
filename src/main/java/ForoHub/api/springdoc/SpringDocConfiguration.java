440877af8f4e0582da97621bd1c17d8d7c563ec3
BFeer <bfmg69@gmail.com>
2024-07-20 11:49:38 -0600

    Actualización de configuración para documentación

--- src/main/java/ForoHub/api/springdoc/SpringDocConfiguration.java
+++ src/main/java/ForoHub/api/springdoc/SpringDocConfiguration.java
@@ -1,9 +1,12 @@
 package ForoHub.api.springdoc;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,6 +24,12 @@ public class SpringDocConfiguration {
                                 .email("bfmg69@gmail.com"))
                         .license(new License()
                                 .name("Apache 2.0")
-                                .url("http://voll.med/api/licencia")));
+                                .url("http://voll.med/api/licencia")))
+                .addSecurityItem(new SecurityRequirement().addList("basic-auth"))
+                .components(new Components()
+                        .addSecuritySchemes("basic-auth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("basic")));
     }
 }
